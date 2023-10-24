9fe798ce2fdd4796104265b0fae0a6970f96cc3c
aadithya78 <131513619+aadithya78@users.noreply.github.com>
2023-10-24 08:31:52 +0530

    Add files via upload

---
+++ CodeAlpha Personal Assistant/main.py
@@ -0,0 +1,160 @@
+import requests
+from functions.online_ops import find_my_ip, get_random_advice, get_random_joke, play_on_youtube, search_on_google, search_on_wikipedia,  send_whatsapp_message
+import pyttsx3
+import speech_recognition as sr
+from decouple import config
+from datetime import datetime
+from functions.os_ops import open_calculator, open_camera, open_cmd, open_notepad, open_discord , open_hazel
+from random import choice
+from utils import opening_text
+from pprint import pprint
+import time
+
+
+USERNAME = config('USER')
+BOTNAME = config('BOTNAME')
+
+engine = pyttsx3.init('sapi5')
+
+# Set Rate
+
+engine.setProperty('rate', 190)
+
+# Set Volume
+engine.setProperty('volume', 1.0)
+
+# Set Voice (Female)
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+# Text to Speech Conversion
+def speak(text):
+    """Used to speak whatever text is passed to it"""
+
+    engine.say(text)
+    engine.runAndWait()
+
+
+# Greet the user
+def greet_user():
+    """Greets the user according to the time"""
+    
+    hour = datetime.now().hour
+    if (hour >= 6) and (hour < 12):
+        speak(f"Good Morning {USERNAME}")
+    elif (hour >= 12) and (hour < 16):
+        speak(f"Good afternoon {USERNAME}")
+    elif (hour >= 16) and (hour < 19):
+        speak(f"Good Evening {USERNAME}")
+    speak(f"I am {BOTNAME}. How may I assist you?")
+
+
+# Takes Input from User
+def take_user_input():
+    """Takes user input, recognizes it using Speech Recognition module and converts it into text"""
+    
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening....')
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print('Recognizing...')
+        query = r.recognize_google(audio, language='en-in')
+        if not 'exit' in query or 'stop' in query:
+            speak(choice(opening_text))
+        else:
+            hour = datetime.now().hour
+            if hour >= 21 and hour < 6:
+                speak("Good night sir, take care!")
+            else:
+                speak('Have a good day sir!')
+            exit()
+    except Exception:
+        speak('Sorry, I could not understand. Could you please say that again?')
+        query = 'None'
+    return query
+
+
+if __name__ == '__main__':
+    greet_user()
+    while True:
+        query = take_user_input().lower()
+
+        if 'open notepad' in query:
+            open_notepad()
+            
+        elif 'tell about yourself' in query:
+            open_hazel('HAZEL.jpg')
+            time.sleep(10)
+            speak('I am HAZEL , an AI assistant developed and trained by Aadetheeya , and he is a student of Sri Ram Engineering college located at Perumalpattu , He owns a youtube channel called DIGI TRONIX , Thank you ,  anything else , sir ?' )
+            
+        elif 'tell about chitti' in query:
+            speak('Hahaha , chittie is a humanoid robot developed by Doctor vaseegaran in Endhiran movie')            
+        elif 'open discord' in query:
+            open_discord()
+            
+        elif 'time now' in query:
+            hour = datetime.now().hour
+            speak(f'The time is {hour}')
+
+
+        elif 'open command prompt' in query or 'open cmd' in query:
+            open_cmd()
+
+        elif 'open camera' in query:
+            open_camera()
+
+        elif 'open calculator' in query:
+            open_calculator()
+
+        elif 'ip address' in query:
+            ip_address = find_my_ip()
+            speak(f'Your IP Address is {ip_address}.\n For your convenience, I am printing it on the screen sir.')
+            print(f'Your IP Address is {ip_address}')
+
+        elif 'wikipedia' in query:
+            speak('What do you want to search on Wikipedia, sir?')
+            search_query = take_user_input().lower()
+            results = search_on_wikipedia(search_query)
+            speak(f"According to Wikipedia, {results}")
+            if 'stop' in query:
+                speak('ok sir i am stopping my answer')
+            else:
+                speak("For your convenience, I am printing it on the screen sir")
+            print(results)
+
+        elif 'youtube' in query:
+            speak('What do you want to play on Youtube, sir?')
+            video = take_user_input().lower()
+            play_on_youtube(video)
+
+        elif 'search on google' in query:
+            speak('What do you want to search on Google, sir?')
+            query = take_user_input().lower()
+            search_on_google(query)
+
+        elif "send whatsapp message" in query:
+            speak(
+                'On what number should I send the message sir? Please enter in the console: ')
+            number = input("Enter the number: ")
+            speak("What is the message sir?")
+            message = take_user_input().lower()
+            send_whatsapp_message(number, message)
+            speak("I've sent the message sir.")
+
+        elif 'joke' in query:
+            speak(f"Hope you like this one sir")
+            joke = get_random_joke()
+            speak(joke)
+            speak("For your convenience, I am printing it on the screen sir.")
+            pprint(joke)
+
+        elif "advice" in query:
+            speak(f"Here's an advice for you, sir")
+            advice = get_random_advice()
+            speak(advice)
+            speak("For your convenience, I am printing it on the screen sir.")
+            pprint(advice)
